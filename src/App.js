6615478a3b3454b476c730afe6c3587af29e4d25
Muhammad Bilal <billu97.memon@gmail.com>
2020-06-08 03:37:46 -0700

    updated app.js file
    created props with disstructuring fashion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ myName, age }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <p>My name is
+        <strong> {myName} </strong>
+       & age is {age} </p>
+      <br />
+      <p>First React app ever Created</p>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App myName="Muhammad Bilal" age={23} />
   </React.StrictMode>,
   document.getElementById('root')
 );
